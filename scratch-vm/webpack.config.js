5ae51c590a3921e54952446a3e284fb7d916a36a
Niarbon <124090704@link.cuhk.edu.cn>
2026-06-26 10:41:21 +0800

    fix: add noErrorOnMissing for optional assets in scratch-vm build

--- scratch-vm/webpack.config.js
+++ scratch-vm/webpack.config.js
@@ -125,11 +125,17 @@ module.exports = [
                 from: 'node_modules/scratch-blocks/media',
                 to: 'media'
             }, {
-                from: 'node_modules/scratch-storage/dist/web'
+                from: 'node_modules/scratch-storage/dist/web',
+                to: 'web/dist',
+                noErrorOnMissing: true
             }, {
-                from: 'node_modules/scratch-render/dist/web'
+                from: 'node_modules/scratch-render/dist/web',
+                to: 'web/dist',
+                noErrorOnMissing: true
             }, {
-                from: 'node_modules/@turbowarp/scratch-svg-renderer/dist/web'
+                from: 'node_modules/@turbowarp/scratch-svg-renderer/dist/web',
+                to: 'web/dist',
+                noErrorOnMissing: true
             }, {
                 from: 'src/playground'
             }])
